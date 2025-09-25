d7742b3660bb49c9b03773383209cf82ec463b7c
Andrew Atkinson <andyatkinson@gmail.com>
2025-09-24 19:57:21 -0500

    Clean up scripts
    Use shellcheck to find unused items to remove

--- pgconfnyc2025/connect_user.sh
+++ pgconfnyc2025/connect_user.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-DB_SUPERUSER="postgres"
-DB_POSTGRES="postgres"
-DB_SUPERUSER_PWD="postgres"
 DB_HOST="localhost"
 DB_PORT="15432"
-
 DB_USER="pgconf"
 DB_PASSWORD="pgconf"
 DB_NAME="pgconf"
-DB_SCHEMA="pgconf"
 
 connect_user() {
   PGPASSWORD=$DB_PASSWORD psql \
